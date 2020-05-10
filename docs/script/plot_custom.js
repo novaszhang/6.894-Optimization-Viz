459fc6deb30a3b28427e36ce406101800867455e
Nova Zhang <novaszhang@gmail.com>
2020-05-10 08:34:34 -0400

    adjust custom gradient

--- docs/script/plot_custom.js
+++ docs/script/plot_custom.js
@@ -28,11 +28,12 @@ var func,
 
 function plot_contour() {
   /* Value of f at (x, y) */
-  func = document.getElementById("func").value.toString()
+  func = document.getElementById("funcInput").value.toString()
   grad_x = math.derivative(func, 'x').toString()
   grad_y = math.derivative(func, 'y').toString()
   console.log(grad_x)
-
+  console.log(grad_y)
+  console.log("contourcontour")
   // Parameters describing where function is defined
   domain_x = [document.getElementById("xmin").value, document.getElementById("xmax").value]
   domain_y = [document.getElementById("ymin").value, document.getElementById("ymax").value]
@@ -145,8 +146,6 @@ function get_f_values(nx, ny, func) {
 /* Returns gradient of f at (x, y) */
 function grad_f(x,y, grad_x, grad_y) {
   let scope = {x:x, y:y}
-  console.log(grad_x)
-  console.log(grad_y)
   var g_x = parseFloat(math.evaluate(grad_x, scope))
   var g_y = parseFloat(math.evaluate(grad_y, scope))
   return [g_x, g_y];
@@ -176,53 +175,217 @@ function button_press() {
  * SGD, Momentum, RMSProp, Adam.
  */
 
- function get_sgd_path(x0, y0, learning_rate) {
+ //no numsteps
+//  function get_sgd_path(x0, y0, learning_rate) {
+//   var sgd_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
+//   var x1, y1, gradient;
+//   var x1 = 0;
+//   var y1 = 0; 
+//   var gradient = [1,1];
+//   while (math.norm(gradient) > 1e-2) {
+//     gradient = grad_f(x0, y0, grad_x, grad_y);
+//     x1 = x0 - learning_rate * gradient[0]
+//     y1 = y0 - learning_rate * gradient[1]
+//     sgd_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
+//     x0 = x1
+//     y0 = y1
+//   }
+//   return sgd_history;
+// }
+
+// function get_momentum_path(x0, y0, learning_rate, momentum) {
+//   var v_x = 0,
+//   v_y = 0;
+//   var momentum_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
+//   var x1 = 0;
+//   var y1 = 0; 
+//   var gradient = [1,1];
+//   while (math.norm(gradient) > 1e-2) {
+//   //for (i=0; i < num_steps; i++) {
+//     gradient = grad_f(x0, y0, grad_x, grad_y)
+//     v_x = momentum * v_x - learning_rate * gradient[0]
+//     v_y = momentum * v_y - learning_rate * gradient[1]
+//     x1 = x0 + v_x
+//     y1 = y0 + v_y
+//     momentum_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
+//     x0 = x1
+//     y0 = y1
+//   }
+//   return momentum_history
+// }
+
+// function get_rmsprop_path(x0, y0, learning_rate, decay_rate, eps) {
+//   var cache_x = 0,
+//   cache_y = 0;
+//   var rmsprop_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
+//   var x1 = 0;
+//   var y1 = 0; 
+//   var gradient = [1,1];
+//   while (math.norm(gradient) > 1e-2) {
+//     gradient = grad_f(x0, y0, grad_x, grad_y)
+//     cache_x = decay_rate * cache_x + (1 - decay_rate) * gradient[0] * gradient[0]
+//     cache_y = decay_rate * cache_y + (1 - decay_rate) * gradient[1] * gradient[1]
+//     x1 = x0 - learning_rate * gradient[0] / (Math.sqrt(cache_x) + eps)
+//     y1 = y0 - learning_rate * gradient[1] / (Math.sqrt(cache_y) + eps)
+//     rmsprop_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
+//     x0 = x1
+//     y0 = y1
+//   }
+//   return rmsprop_history;
+// }
+
+// function get_adam_path(x0, y0, lr, beta_1, beta_2, eps) {
+//   var m_x = 0,
+//   m_y = 0,
+//   v_x = 0,
+//   v_y = 0;
+//   var adam_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
+//   var x1 = 0;
+//   var y1 = 0; 
+//   var gradient = [1,1];
+//   while (math.norm(gradient) > 1e-2) {
+//  // for (i = 0; i < num_steps; i++) {
+//     gradient = grad_f(x0, y0, grad_x, grad_y)
+//     m_x = beta_1 * m_x + (1 - beta_1) * gradient[0]
+//     m_y = beta_1 * m_y + (1 - beta_1) * gradient[1]
+//     v_x = beta_2 * v_x + (1 - beta_2) * gradient[0] * gradient[0]
+//     v_y = beta_2 * v_y + (1 - beta_2) * gradient[1] * gradient[1]
+//     x1 = x0 - lr * m_x / (Math.sqrt(v_x) + eps)
+//     y1 = y0 - lr * m_y / (Math.sqrt(v_y) + eps)
+//     adam_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
+//     x0 = x1
+//     y0 = y1
+//   }
+//   return adam_history;
+// }
+
+
+// function draw_path(path_data, type) {
+//   var gradient_path = gradient_path_g.selectAll(type)
+//   .data(path_data)
+//   .enter()
+//   .append("path")
+//   .attr("d", line_function(path_data.slice(0,1)))
+//   .attr("class", type)
+//   .attr("stroke-width", 3)
+//   .attr("fill", "none")
+//   .attr("stroke-opacity", 0.5)
+//   .transition()
+//   .duration(drawing_time)
+//   .delay(function(d,i) { return drawing_time * i; })
+//   .attr("d", function(d,i) { return line_function(path_data.slice(0,i+1));})
+//   .remove();
+
+//   gradient_path_g.append("path")
+//   .attr("d", line_function(path_data))
+//   .attr("class", type)
+//   .attr("stroke-width", 3)
+//   .attr("fill", "none")
+//   .attr("stroke-opacity", 0.5)
+//   .attr("stroke-opacity", 0)
+//   .transition()
+//   .duration(path_data.length * drawing_time)
+//   .attr("stroke-opacity", 0.5);
+// }
+
+// /*
+//  * Start minimization from click on contour map
+//  */
+
+//  function mousedown() {
+//   /* Get initial point */
+//   var point = d3.mouse(this);
+//  /* Minimize and draw paths */
+//  var sgd_lr = document.getElementById("SGD_lr").value;
+//  var mom_lr = document.getElementById("moment_lr").value;
+//  var rms_lr = document.getElementById("rms_lr").value;
+//  var adam_lr = document.getElementById("adam_lr").value;
+//  minimize(
+//   scale_x(point[0]),
+//   scale_y(point[1]),
+//   sgd_lr,
+//   mom_lr,
+//   rms_lr,
+//   adam_lr);
+// }
+
+// function minimize(
+//       x0,
+//       y0, 
+//       sgd_lr,
+//        mom_lr,
+//        rms_lr,
+//        adam_lr) {
+//   gradient_path_g.selectAll("path").remove();
+
+//   if (draw_bool.SGD) {
+//     var sgd_data = get_sgd_path(x0, y0, sgd_lr-2);
+//     draw_path(sgd_data, "sgd");
+//   }
+//   if (draw_bool.Momentum) {
+//     var momentum_data = get_momentum_path(x0, y0, mom_lr, 0.8);
+//     draw_path(momentum_data, "momentum");
+//   }
+//   if (draw_bool.RMSProp) {
+//     var rmsprop_data = get_rmsprop_path(x0, y0, rms_lr, 0.99, 1e-6);
+//     draw_path(rmsprop_data, "rmsprop");
+//   }
+//   if (draw_bool.Adam) {
+//     var adam_data = get_adam_path(x0, y0, adam_lr, 0.7, 0.999, 1e-6);
+//     draw_path(adam_data, "adam");
+//   }
+// }
+
+function get_sgd_path(x0, y0, learning_rate, num_steps) {
   var sgd_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
   var x1, y1, gradient;
-  var x1 = 0;
-  var y1 = 0; 
-  var gradient = [1,1];
-  while (math.norm(gradient) > 1e-2) {
+  var i=0;
+  while (i < num_steps && math.norm(gradient) > 1e-2) {
     gradient = grad_f(x0, y0, grad_x, grad_y);
     x1 = x0 - learning_rate * gradient[0]
     y1 = y0 - learning_rate * gradient[1]
     sgd_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
     x0 = x1
     y0 = y1
+
+    i++;
   }
   return sgd_history;
 }
 
-function get_momentum_path(x0, y0, learning_rate, momentum) {
+function get_momentum_path(x0, y0, learning_rate, num_steps, momentum) {
   var v_x = 0,
-  v_y = 0;
+      v_y = 0;
   var momentum_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
-  var x1 = 0;
-  var y1 = 0; 
-  var gradient = [1,1];
-  while (math.norm(gradient) > 1e-2) {
-  //for (i=0; i < num_steps; i++) {
-    gradient = grad_f(x0, y0, grad_x, grad_y)
-    v_x = momentum * v_x - learning_rate * gradient[0]
-    v_y = momentum * v_y - learning_rate * gradient[1]
-    x1 = x0 + v_x
-    y1 = y0 + v_y
+  var x1, y1, gradient;
+  while (i < num_steps && math.norm(gradient) > 1e-2) {
+    gradient = grad_f(x0, y0, grad_x, grad_y);
+    
+    v_x = parseFloat(momentum * v_x - learning_rate * gradient[0])
+    v_y = parseFloat(momentum * v_y - learning_rate * gradient[1])
+
+    x1 = parseFloat(x0) + v_x
+    y1 = parseFloat(y0) + v_y
+
     momentum_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
+    
     x0 = x1
     y0 = y1
+
+    i++;
   }
+  console.log("momentum history::::");
+  console.log(momentum_history);
   return momentum_history
 }
 
-function get_rmsprop_path(x0, y0, learning_rate, decay_rate, eps) {
+function get_rmsprop_path(x0, y0, learning_rate, num_steps, decay_rate, eps) {
   var cache_x = 0,
-  cache_y = 0;
+      cache_y = 0;
   var rmsprop_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
-  var x1 = 0;
-  var y1 = 0; 
-  var gradient = [1,1];
-  while (math.norm(gradient) > 1e-2) {
-    gradient = grad_f(x0, y0, grad_x, grad_y)
+  var x1, y1, gradient;
+  while (i < num_steps && math.norm(gradient) > 1e-2) {
+    gradient = grad_f(x0, y0, grad_x, grad_y);
     cache_x = decay_rate * cache_x + (1 - decay_rate) * gradient[0] * gradient[0]
     cache_y = decay_rate * cache_y + (1 - decay_rate) * gradient[1] * gradient[1]
     x1 = x0 - learning_rate * gradient[0] / (Math.sqrt(cache_x) + eps)
@@ -230,108 +393,130 @@ function get_rmsprop_path(x0, y0, learning_rate, decay_rate, eps) {
     rmsprop_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
     x0 = x1
     y0 = y1
+
+    i++;
   }
   return rmsprop_history;
 }
 
-function get_adam_path(x0, y0, lr, beta_1, beta_2, eps) {
+function get_adam_path(x0, y0, learning_rate, num_steps, beta_1, beta_2, eps) {
   var m_x = 0,
-  m_y = 0,
-  v_x = 0,
-  v_y = 0;
+      m_y = 0,
+      v_x = 0,
+      v_y = 0;
   var adam_history = [{"x": scale_x.invert(x0), "y": scale_y.invert(y0)}];
-  var x1 = 0;
-  var y1 = 0; 
-  var gradient = [1,1];
-  while (math.norm(gradient) > 1e-2) {
- // for (i = 0; i < num_steps; i++) {
-    gradient = grad_f(x0, y0, grad_x, grad_y)
+  var x1, y1, gradient;
+  while (i < num_steps && math.norm(gradient) > 1e-2) {
+    gradient = grad_f(x0, y0, grad_x, grad_y);
     m_x = beta_1 * m_x + (1 - beta_1) * gradient[0]
     m_y = beta_1 * m_y + (1 - beta_1) * gradient[1]
     v_x = beta_2 * v_x + (1 - beta_2) * gradient[0] * gradient[0]
     v_y = beta_2 * v_y + (1 - beta_2) * gradient[1] * gradient[1]
-    x1 = x0 - lr * m_x / (Math.sqrt(v_x) + eps)
-    y1 = y0 - lr * m_y / (Math.sqrt(v_y) + eps)
+    x1 = x0 - learning_rate * m_x / (Math.sqrt(v_x) + eps)
+    y1 = y0 - learning_rate * m_y / (Math.sqrt(v_y) + eps)
     adam_history.push({"x" : scale_x.invert(x1), "y" : scale_y.invert(y1)})
     x0 = x1
     y0 = y1
+    i++;
   }
   return adam_history;
 }
 
 
+/*
+* Functions necessary for path visualizations
+*/
+
+var line_function = d3.line()
+                    .x(function(d) { return d.x; })
+                    .y(function(d) { return d.y; });
+
 function draw_path(path_data, type) {
   var gradient_path = gradient_path_g.selectAll(type)
-  .data(path_data)
-  .enter()
-  .append("path")
-  .attr("d", line_function(path_data.slice(0,1)))
-  .attr("class", type)
-  .attr("stroke-width", 3)
-  .attr("fill", "none")
-  .attr("stroke-opacity", 0.5)
-  .transition()
-  .duration(drawing_time)
-  .delay(function(d,i) { return drawing_time * i; })
-  .attr("d", function(d,i) { return line_function(path_data.slice(0,i+1));})
-  .remove();
+                      .data(path_data)
+                      .enter()
+                      .append("path")
+                      .attr("d", line_function(path_data.slice(0,1)))
+                      .attr("class", type)
+                      .attr("stroke-width", 3)
+                      .attr("fill", "none")
+                      .attr("stroke-opacity", 0.5)
+                      .transition()
+                      .duration(drawing_time)
+                      .delay(function(d,i) { return drawing_time * i; })
+                      .attr("d", function(d,i) { return line_function(path_data.slice(0,i+1));})
+                      .remove();
 
   gradient_path_g.append("path")
-  .attr("d", line_function(path_data))
-  .attr("class", type)
-  .attr("stroke-width", 3)
-  .attr("fill", "none")
-  .attr("stroke-opacity", 0.5)
-  .attr("stroke-opacity", 0)
-  .transition()
-  .duration(path_data.length * drawing_time)
-  .attr("stroke-opacity", 0.5);
+                 .attr("d", line_function(path_data))
+                 .attr("class", type)
+                 .attr("stroke-width", 3)
+                 .attr("fill", "none")
+                 .attr("stroke-opacity", 0.5)
+                 .attr("stroke-opacity", 0)
+                 .transition()
+                 .duration(path_data.length * drawing_time)
+                 .attr("stroke-opacity", 0.5);
 }
 
 /*
- * Start minimization from click on contour map
- */
+* Start minimization from click on contour map
+*/
 
- function mousedown() {
+function mousedown() {
   /* Get initial point */
   var point = d3.mouse(this);
- /* Minimize and draw paths */
- var sgd_lr = document.getElementById("SGD_lr").value;
- var mom_lr = document.getElementById("moment_lr").value;
- var rms_lr = document.getElementById("rms_lr").value;
- var adam_lr = document.getElementById("adam_lr").value;
- minimize(
-  scale_x(point[0]),
-  scale_y(point[1]),
-  sgd_lr,
-  mom_lr,
-  rms_lr,
-  adam_lr);
+  /* Minimize and draw paths */
+  var sgd_lr = document.getElementById("SGD_lr").value;
+  var mom_lr = document.getElementById("moment_lr").value;
+  var rms_lr = document.getElementById("rms_lr").value;
+  var adam_lr = document.getElementById("adam_lr").value;
+  minimize(
+    scale_x(point[0]),
+     scale_y(point[1]),
+     sgd_lr,
+     mom_lr,
+     rms_lr,
+     adam_lr);
 }
 
 function minimize(
-      x0,
-      y0, 
-      sgd_lr,
-       mom_lr,
-       rms_lr,
-       adam_lr) {
+    x0,
+    y0, 
+    sgd_lr,
+     mom_lr,
+     rms_lr,
+     adam_lr) {
   gradient_path_g.selectAll("path").remove();
 
   if (draw_bool.SGD) {
-    var sgd_data = get_sgd_path(x0, y0, sgd_lr-2);
-    draw_path(sgd_data, "sgd");
+      var sgd_data = get_sgd_path(x0, y0, sgd_lr, 500);
+      draw_path(sgd_data, "sgd");
   }
   if (draw_bool.Momentum) {
-    var momentum_data = get_momentum_path(x0, y0, mom_lr, 0.8);
-    draw_path(momentum_data, "momentum");
+      var momentum_data = get_momentum_path(x0, y0, mom_lr, 700, 0.8);
+      draw_path(momentum_data, "momentum");
   }
   if (draw_bool.RMSProp) {
-    var rmsprop_data = get_rmsprop_path(x0, y0, rms_lr, 0.99, 1e-6);
-    draw_path(rmsprop_data, "rmsprop");
+      var rmsprop_data = get_rmsprop_path(x0, y0, rms_lr, 700, 0.99, 1e-6);
+      draw_path(rmsprop_data, "rmsprop");
   }
   if (draw_bool.Adam) {
-    var adam_data = get_adam_path(x0, y0, adam_lr, 0.7, 0.999, 1e-6);
-    draw_path(adam_data, "adam");
+      var adam_data = get_adam_path(x0, y0, adam_lr, 700, 0.7, 0.999, 1e-6);
+      draw_path(adam_data, "adam");
   }
+}
+
+function plotter(erase) {
+  if (erase) {
+    d3.selectAll("g > *").remove()
+    svg.remove();
+    // func = document.getElementById("funcInput").value.toString()
+
+    // console.log(func)
+    // grad_x = math.derivative(func, 'x').toString()
+    // console.log(grad_x)
+  }
+  plot_contour();
+//plot_3d();
 }
